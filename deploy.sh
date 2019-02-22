aa2a3cbb7775209cce1d500fb85f1968fcc352d2
vbalasubramaniam <vbalasubramaniam@trifacta.com>
2019-02-21 16:33:21 -0800

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 if [ "$(hostname)" = "VijayB-MacBook-Pro-43244.local" ]; then
-  echo "local"
+  echo "running local steps"
+  cp /Users/vbalasubramaniam/udfs/DatarobotPredict/out/artifacts/DatarobotPredict_jar/DatarobotPredict.jar .
+  git add DatarobotPredict.jar
+  git commit -m "Updated DatarobotPredict.jar"
+  git push
 else
-  echo "remote"
+  echo "running remote steps"
+  git pull
+  chown trifacta:trifacta DatarobotPredict.jar
+  cp DatarobotPredict.jar /udfs/
+  service trifacta restart
 fi
